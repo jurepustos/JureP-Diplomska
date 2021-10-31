da268ab21851b44b5553ed7ac398eb5922eb4168
Jure Pustoslemsek <jure.poglednik@gmail.com>
2021-10-31 03:19:29 +0100

    small fix before going to sleep

--- dlx/src/dlx_table.rs
+++ dlx/src/dlx_table.rs
@@ -151,28 +151,68 @@ impl DLXTable {
 
     pub fn cover_element(&mut self, elem_index: usize) {
         if elem_index < self.elements.len() {
-            let header_index = elem_index+1;
-            self.hide_element(header_index);
-            for node_index in self.element_nodes(header_index) {
+            self.hide_element(elem_index);
+            for node_index in self.element_nodes(elem_index) {
                 self.hide_row(node_index);
             }
         }
     }
 
-    fn hide_element(&mut self, index: usize) {
-
+    fn hide_element(&mut self, elem_index: usize) {
+        if elem_index < self.elements.len() {
+            let header_node = &self.nodes[elem_index+1];
+            let mut left_node = &mut self.nodes[header_node.left];
+            let mut right_node = &mut self.node[header_node.right];
+            left_node.right = header_node.right;
+            right_node.left = header_node.left;
+        }
     }
 
-    fn element_nodes(&self, index: usize) -> Vec<usize> {
+    fn element_nodes(&self, elem_index: usize) -> Vec<usize> {
+        let mut indices = Vec::new();
+        if elem_index < self.elements.len() {
+            let header_index = elem_index+1;
+            let mut next_node = &self.nodes[header_index];
+            while next_node.down != header_index {
+                indices.push(next_node.down);
+                next_node = &mut self.nodes[next_node.down];
+            }
+        }
 
+        indices
     }
 
-    fn hide_row(&mut self, index: usize) {
+    fn row_nodes(&self, node_index: usize) -> Vec<usize> {
+        let mut indices = Vec::new();
+        if let Some(start_node) = self.nodes.get_mut(node_index) {
+            let mut next_node = start_node;
+            while next_node.right != node_index {
+                indices.push(next_node.right);
+                next_node = &mut self.nodes[next_node.right];
+            }
+        }
 
+        indices
     }
 
+    fn hide_row(&mut self, node_index: usize) {
+        for index in self.row_nodes(node_index) {
+            let node = &self.nodes[index];
+            let mut up_node = &mut self.nodes[node.up];
+            let mut down_node = &mut self.nodes[node.down];
+            let mut header_node = &mut self.nodes[node.header];
+            up_node.down = node.down;
+            down_node.up = node.up;
+            header_node.len -= 1;
+        }
+    }
+
+    // TODO: not correctly implemented yet
     pub fn cover_row(&mut self, node_index: usize) {
-        
+        for index in self.row_nodes(node_index) {
+            let node = &self.nodes[index];
+            self.cover_element(node.header-1);
+        }
     }
 
     pub fn uncover_element(&mut self, elem_index: usize) {
@@ -184,11 +224,11 @@ impl DLXTable {
     }
 
     fn node_visible(&self, node_index: usize) -> bool {
-        if let Some(node) = self.nodes.get(node_index) {
-            let left_node = self.nodes[node.left];
-            let right_node = self.nodes[node.right];
-            let up_node = self.nodes[node.up];
-            let down_node = self.nodes[node.down];
+        if let Some(node) = &self.nodes.get(node_index) {
+            let left_node = &self.nodes[node.left];
+            let right_node = &self.nodes[node.right];
+            let up_node = &self.nodes[node.up];
+            let down_node = &self.nodes[node.down];
 
             left_node.right == node_index && right_node.left == node_index
                 && up_node.down == node_index && down_node.up == node_index
