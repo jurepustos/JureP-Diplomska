6f09080235dde5b751bcf1f5900bdfd9a6169266
Jure Pustoslemšek <jure.poglednik@gmail.com>
2022-04-09 01:40:36 +0200

    untrack libdlx/target

--- dlx/libdlx/target/rls/debug/deps/libdlx-a626468c18ef38e9.d
+++
@@ -1,7 +0,0 @@
-/mnt/d/Documents and projects/Faks/Diplomska/dlx/libdlx/target/rls/debug/deps/libdlx-a626468c18ef38e9.rmeta: libdlx/src/lib.rs libdlx/src/dlx_table.rs libdlx/src/dlx.rs
-
-/mnt/d/Documents and projects/Faks/Diplomska/dlx/libdlx/target/rls/debug/deps/libdlx-a626468c18ef38e9.d: libdlx/src/lib.rs libdlx/src/dlx_table.rs libdlx/src/dlx.rs
-
-libdlx/src/lib.rs:
-libdlx/src/dlx_table.rs:
-libdlx/src/dlx.rs:
--- dlx/libdlx/target/rls/debug/deps/libdlx-e65bce79908e773c.d
+++
@@ -1,7 +0,0 @@
-/mnt/d/Documents and projects/Faks/Diplomska/dlx/libdlx/target/rls/debug/deps/libdlx-e65bce79908e773c.rmeta: libdlx/src/lib.rs libdlx/src/dlx_table.rs libdlx/src/dlx.rs
-
-/mnt/d/Documents and projects/Faks/Diplomska/dlx/libdlx/target/rls/debug/deps/libdlx-e65bce79908e773c.d: libdlx/src/lib.rs libdlx/src/dlx_table.rs libdlx/src/dlx.rs
-
-libdlx/src/lib.rs:
-libdlx/src/dlx_table.rs:
-libdlx/src/dlx.rs:
