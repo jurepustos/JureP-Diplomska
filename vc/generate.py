9bef1ab88bfb9abe65cfabbe08c887353727c5ff
Jure Pustoslemšek <jure.poglednik@gmail.com>
2022-07-20 11:26:13 +0200

    generator in instance VC problemov

---
+++ vc/generate.py
@@ -0,0 +1,27 @@
+import os
+import random
+import sys
+import networkx
+from typing import List, Tuple
+
+
+def main():
+    folder = sys.argv[1]
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    for n in range(10, 151, 10):
+        edges_samples = random.sample(range(2*n, n*(n-1)), 10)
+        for m in edges_samples:
+            graph = networkx.gnm_random_graph(n, m)
+            edges = [(x,y) for x,y in graph.edges if x < y]
+            with open(f'{folder}/graph{n}_{m}.dimacs', mode='w+') as file:
+                print(f'{n} {m}')
+                for x,y in edges:
+                    print(x, y, file=file)
+                
+                file.flush()
+
+
+if __name__ == '__main__':
+    main()
---
+++ vc/run_vc.py
@@ -0,0 +1,15 @@
+
+
+import sys
+
+
+def main():
+    algo = sys.argv[1]
+    if algo == 'dlx':
+        pass
+    elif algo == 'vc':
+        pass
+
+
+if __name__ == '__main__':
+    main()
