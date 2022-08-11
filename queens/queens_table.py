34ab667c40c99d78b8fb78b6a45517013189a353
Jure Pustoslemšek <jure.poglednik@gmail.com>
2022-08-11 19:19:50 +0200

    queens tabela

--- queens/queens_table.py
+++ queens/queens_table.py
@@ -1,40 +1,31 @@
 from math import ceil, floor, inf
+from operator import itemgetter
 import sys
 
-
-def parse_int(x):
-    try:
-        return int(x)
-    except:
-        return '-'
-
 def main():
-    filename1 = sys.argv[1]
-    filename2 = sys.argv[2]
-    with open(filename1) as file1, open(filename2) as file2:
-        contents1 = list(map(lambda line: line.split(' '), file1.readlines()))
-        contents2 = list(map(lambda line: line.split(' '), file2.readlines()))
+    filenames = sys.argv[1:]
+    contents = []
+    for filename in filenames:
+        with open(filename) as file:
+            contents.append([line.split(' ') for line in file.readlines()])
 
-    data1 = {int(line[0]): parse_int(line[1]) for line in contents1}
-    data2 = {int(line[0]): parse_int(line[1]) for line in contents2}
+    keys = {int(line[0]) for content in contents for line in content}
 
-    keys = list(set(data1.keys()).union(set(data2.keys())))
-    keys.sort()
+    data_rows = {key: [[int(line[1])
+                    for line in content if int(line[0]) == key] 
+                for content in contents]
+            for key in keys}
 
-    data_rows = []
-    for key in keys:
-        data_rows.append(f'{key} & {data1[key]} & {data2[key]}')
+    avg_rows = [(key, [(round(sum(values) / len(values)) if len(values) >= 7 else '-') for values in row]) 
+                for key, row in data_rows.items()]
+    avg_rows.sort(key=itemgetter(0))
+    for key, avg in avg_rows:
+        print(key, *avg, sep=' & ', end=' \\\\\n')
+    
 
-    third = ceil(len(data_rows) / 3)
-    for i in range(third):
-        print(data_rows[i], '&')
-        print(data_rows[i + third], '&')
-        try:
-            print(data_rows[i + 2*third], '\\\\')
-        except:
-            print('& & \\\\')
+    
+    
 
-    print()
 
 
 if __name__ == '__main__':
